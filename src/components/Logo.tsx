2ca27a375fbe5e9a65e7b201e436ff75042f63ce
brechtdoran <brechtdoran@gmail.com>
2022-09-16 16:31:05 +0200

    Edit Logo responsive

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -7,7 +7,7 @@ const Logo: FC<LogoProps> = ({}) => {
   return (
     <div className="flex items-center gap-x-2">
       <Image src={'/images/monero-logo.png'} height={50} width={50} alt="Monero Logo" />
-      <h1 className="font-display text-2xl">
+      <h1 className="text-md font-display sm:text-2xl">
         <span className="text-secondary">Monero</span> <span className="text-primary">Node Dashboard</span>
       </h1>
     </div>
